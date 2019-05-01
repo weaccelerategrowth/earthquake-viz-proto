acda88090f4be4788a9ea00ca17d4abe004f9063
Joseph Izatt <joe@izatt.co.uk>
2019-05-01 19:19:12 +0100

    Updates

---
+++ precache-manifest.2c26c28613b994f82bd3753f8afd6b78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d509d4575de21683144462e09c42d5",
+    "url": "/earthquake-viz-proto/index.html"
+  },
+  {
+    "revision": "a67ddcf34f2add2d41f9",
+    "url": "/earthquake-viz-proto/static/css/main.2aed0ef8.chunk.css"
+  },
+  {
+    "revision": "05af13ee4ec08841ed00",
+    "url": "/earthquake-viz-proto/static/js/2.86ae3fdd.chunk.js"
+  },
+  {
+    "revision": "a67ddcf34f2add2d41f9",
+    "url": "/earthquake-viz-proto/static/js/main.6b375576.chunk.js"
+  },
+  {
+    "revision": "8dca2cdc3b80eba819a7",
+    "url": "/earthquake-viz-proto/static/js/runtime~main.ab181803.js"
+  }
+]);
